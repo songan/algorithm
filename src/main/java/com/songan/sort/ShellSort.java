88ac12956da00a177e469b594a37adcdec5f5503
songan <wb-sa774167@alibabapictures.com>
2020-12-04 14:34:53 +0800

    Initial commit

---
+++ src/main/java/com/songan/sort/ShellSort.java
@@ -0,0 +1,45 @@
+package com.songan.sort;
+
+import java.util.Arrays;
+
+/**
+ * @author songan
+ * @date 2020/12/03
+ */
+
+public class ShellSort {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{20, 2, 34, 12, 6, 3, 43};
+        shellSort(arr);
+    }
+
+    /**
+     * 希尔排序
+     *
+     * 1. 选择一个增量序列t1，t2，…，tk，其中ti>tj，tk=1；（一般初次取数组半长，之后每次再减半，直到增量为1）
+     * 2. 按增量序列个数k，对序列进行k 趟排序；
+     * 3. 每趟排序，根据对应的增量ti，将待排序列分割成若干长度为m 的子序列，分别对各子表进行直接插入排序。
+     *    仅增量因子为1 时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+     * @param arr  待排序数组
+     */
+    public static void shellSort(int[] arr){
+        int gap = arr.length / 2;
+        //不断缩小gap，直到1为止
+        for (; gap > 0; gap /= 2) {
+            //使用当前gap进行组内插入排序
+            for (int j = 0; (j+gap) < arr.length; j++){
+                for(int k = 0; (k+gap)< arr.length; k += gap){
+                    //交换操作
+                    if(arr[k] > arr[k+gap]) {
+                        int temp = arr[k+gap];
+                        arr[k+gap] = arr[k];
+                        arr[k] = temp;
+                        System.out.println("    Sorting:  " + Arrays.toString(arr));
+                    }
+                }
+            }
+        }
+    }
+
+}
